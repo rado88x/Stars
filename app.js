34bcec57225195010453e778ba8ac033e29ed696
Radoslav Velichkov <rado88x@gmail.com>
2021-12-30 12:08:08 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById("button").addEventListener("click", function(){
+    stars(5);
+});
+
+function stars (n){
+    let result = "";
+      for (let i = 0 ; i < n ; i++){
+          let line = "!";
+          let whiteSpace = (n - i - 1);
+          line += " ".repeat(whiteSpace)
+          let starsCount = 2 * i + 1;
+          line += "*".repeat(starsCount);
+          result += line + "\n";
+          console.log(line);
+      }
+      console.log(result);
+      let htmlResult = result.replaceAll("\n","<br>");
+      console.log("HTML result = " + htmlResult)
+      document.getElementById("divStars").innerHTML = htmlResult;
+}
+
+stars(5);
